9c883939d66bf4d1567a8876e6af32bbcbc38202
Frank Wember <127029813+FrankWember@users.noreply.github.com>
2025-05-11 21:27:42 -0400

    maed the landing page for easy testing

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,44 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className="flex justify-center items-center flex-col gap-10">
-      <Button variant="outline" size="sm">
-        sm button
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={() => router.push("/auth/signin")}
+      >
+        Sign In (sm)
       </Button>
-      <Button variant="destructive" size="lg">
-        large button
+      <Button
+        variant="destructive"
+        size="lg"
+        onClick={() => router.push("/auth/signin")}
+      >
+        Sign In (large)
       </Button>
-      <Button variant="ghost" size="lg">
-        ghost button
+      <Button
+        variant="ghost"
+        size="lg"
+        onClick={() => router.push("/auth/signup")}
+      >
+        Sign Up (ghost)
       </Button>
-      <Button variant="link" size="lg">
-        link button
+      <Button
+        variant="link"
+        size="lg"
+        onClick={() => router.push("/auth/signup")}
+      >
+        Sign Up (link)
       </Button>
-      <Button variant="myButton">My Button</Button>
+      <Button variant="myButton" onClick={() => router.push("/auth/signup")}>
+        Custom Signup
+      </Button>
     </div>
   );
 }
