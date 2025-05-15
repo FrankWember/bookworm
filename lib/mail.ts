54f441a7b88df7f4bce3e4f6bb2ac08b9810c206
Frank Wember <127029813+FrankWember@users.noreply.github.com>
2025-05-15 18:01:09 -0400

    Changed the nex auth url

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const verificationUrl = `http://localhost:3000/auth/new-verification?token=${token}`;
+  const verificationUrl = `${NEXTAUTH_URL}/auth/new-verification?token=${token}`;
   console.log("Sending verification email to:", email);
   await resend.emails.send({
     from: "Bookworm@resend.dev",
@@ -14,7 +14,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetUrl = `http://localhost:3000/auth/reset-password?token=${token}`;
+  const resetUrl = `${NEXTAUTH_URL}/auth/reset-password?token=${token}`;
   console.log("Sending password reset email to:", email);
   await resend.emails.send({
     from: "Bookworm@resend.dev",
